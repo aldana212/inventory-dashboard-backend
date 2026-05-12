eccc48c5faba383ef293267a1465e9b7187ce153
daniel aldana <frontend@thisisvank.com>
2026-05-11 23:13:48 -0500

    fix(auth): validate email

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,12 +1,19 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
+
+// FORZAR IPV4
+dns.setDefaultResultOrder("ipv4first");
 
 export const transporter = nodemailer.createTransport({
   // service: "gmail", // o usa SMTP real en producción
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
